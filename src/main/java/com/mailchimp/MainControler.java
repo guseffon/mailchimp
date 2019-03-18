e676fec55fa972abaadf5c4ac98ffd9fb29f5711
guseff <git07@latecore.com>
2019-03-18 13:05:37 +0200

    ortoi

--- src/main/java/com/mailchimp/MainControler.java
+++ src/main/java/com/mailchimp/MainControler.java
@@ -3,12 +3,13 @@ package com.mailchimp;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MainControler {
 
-    @GetMapping("/test")
+    @PostMapping("/test")
 
     public String getTestPage(@RequestParam(defaultValue = "some name") String name,
                               @RequestParam(defaultValue = "some descr") String descr, Model model){
