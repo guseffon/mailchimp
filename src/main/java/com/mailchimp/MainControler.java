4dbed2579830034cb5377df008ad3b0ee14e948d
guseff <git07@latecore.com>
2019-03-18 13:24:29 +0200

    ortoi

--- src/main/java/com/mailchimp/MainControler.java
+++ src/main/java/com/mailchimp/MainControler.java
@@ -11,11 +11,14 @@ public class MainControler {
 
     @PostMapping("/test")
 
-    public String getTestPage(@RequestParam(defaultValue = "some name") String name,
+    public String postTestPage(@RequestParam(defaultValue = "some name") String name,
                               @RequestParam(defaultValue = "some descr") String descr, Model model){
 
         model.addAttribute("name" ,name);
         model.addAttribute("descr" ,descr);
+        return "test";
+    }
+    public String getTestPage(){
         return "test";
     }
 }
